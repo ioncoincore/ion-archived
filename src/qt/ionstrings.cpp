42a8628ac4d05983f64f341bbe8fb124d9ffc12b
Fuzzbawls <fuzzbawls@gmail.com>
2018-12-05 15:28:57 -0800

    Periodic translation update
    simple `make translate` to update source locale files for pushing to
    transifex.
    
    Signed-off-by: cevap <dev@i2pmail.org>

--- src/qt/ionstrings.cpp
+++ src/qt/ionstrings.cpp
@@ -192,6 +192,9 @@ QT_TRANSLATE_NOOP("ion-core", ""
 "the OpenSSL Toolkit <https://www.openssl.org/> and cryptographic software "
 "written by Eric Young and UPnP software written by Thomas Bernard."),
 QT_TRANSLATE_NOOP("ion-core", ""
+"Total length of network version string (%i) exceeds maximum length (%i). "
+"Reduce the number or size of uacomments."),
+QT_TRANSLATE_NOOP("ion-core", ""
 "Unable to bind to %s on this computer. Ion Core is probably already running."),
 QT_TRANSLATE_NOOP("ion-core", ""
 "Unable to locate enough Obfuscation denominated funds for this transaction."),
@@ -247,6 +250,7 @@ QT_TRANSLATE_NOOP("ion-core", "Add a node to connect to and attempt to keep the 
 QT_TRANSLATE_NOOP("ion-core", "Allow DNS lookups for -addnode, -seednode and -connect"),
 QT_TRANSLATE_NOOP("ion-core", "Already have that input."),
 QT_TRANSLATE_NOOP("ion-core", "Always query for peer addresses via DNS lookup (default: %u)"),
+QT_TRANSLATE_NOOP("ion-core", "Append comment to the user agent string"),
 QT_TRANSLATE_NOOP("ion-core", "Attempt to force blockchain corruption recovery"),
 QT_TRANSLATE_NOOP("ion-core", "Attempt to recover private keys from a corrupt wallet.dat"),
 QT_TRANSLATE_NOOP("ion-core", "Automatically create Tor hidden service (default: %d)"),
@@ -498,6 +502,7 @@ QT_TRANSLATE_NOOP("ion-core", "Use UPnP to map the listening port (default: %u)"
 QT_TRANSLATE_NOOP("ion-core", "Use UPnP to map the listening port (default: 1 when listening)"),
 QT_TRANSLATE_NOOP("ion-core", "Use a custom max chain reorganization depth (default: %u)"),
 QT_TRANSLATE_NOOP("ion-core", "Use the test network"),
+QT_TRANSLATE_NOOP("ion-core", "User Agent comment (%s) contains unsafe characters."),
 QT_TRANSLATE_NOOP("ion-core", "Username for JSON-RPC connections"),
 QT_TRANSLATE_NOOP("ion-core", "Value is below the smallest available denomination (= 1) of xION"),
 QT_TRANSLATE_NOOP("ion-core", "Value more than Obfuscation pool maximum allows."),
