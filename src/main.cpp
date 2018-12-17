33cb56773119e91f78800c021da1b769f1e9725b
cevap <dev@i2pmail.org>
2018-12-17 04:50:10 +0100

    Fix sync
    Skip signature verification during initial block download,
    or if blockheight is smaller than zerocoin v2 startheight

--- src/main.cpp
+++ src/main.cpp
@@ -992,8 +992,8 @@ bool CheckZerocoinSpend(const CTransaction& tx, bool fVerifySignature, CValidati
         if (newSpend.getTxOutHash() != hashTxOut)
             return state.DoS(100, error("Zerocoinspend does not use the same txout that was used in the SoK"));
 
-        // Skip signature verification during initial block download
-        if (fVerifySignature) {
+        // Skip signature verification during initial block download, or if blockheight is smaller than zerocoin v2 startheight
+        if ( fVerifySignature || chainActive.Height() < Params().Zerocoin_Block_V2_Start()) {
             //see if we have record of the accumulator used in the spend tx
             CBigNum bnAccumulatorValue = 0;
             if (!zerocoinDB->ReadAccumulatorValue(newSpend.getAccumulatorChecksum(), bnAccumulatorValue)) {
