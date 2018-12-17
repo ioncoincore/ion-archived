d47ce1947f135b3a7986ccc6ea6f1f621cc73961
cevap <dev@i2pmail.org>
2018-12-17 14:31:28 +0100

    Change BIP44 for ION
    Previous setting is used by PIVX and we can not register it until we change
    ION converted from Text into Decimal is 73 79 78, thats why I choose it
    
    More info about it:
    https://github.com/satoshilabs/slips/blob/master/slip-0044.md

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -218,7 +218,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
         // 	BIP44 coin type is from https://github.com/satoshilabs/slips/blob/master/slip-0044.md
-        base58Prefixes[EXT_COIN_TYPE] = boost::assign::list_of(0x80)(0x00)(0x00)(0x77).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_COIN_TYPE] = boost::assign::list_of(0x80)(0x73)(0x79)(0x78).convert_to_container<std::vector<unsigned char> >();
 
         bech32_hrp = "ion";
 
